ade3a7e00cf2e8c32b5f6f1b63080e246697b6c5
jigsaw237 <snigdhamundra2@gmail.com>
2025-05-08 13:06:10 +1200

    Simple search bar with course code and course name matching (no styling)

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,39 @@
 import React, { useState } from 'react';
 import "./SearchBar.css";
+import {data} from '../data.js'
 
 const SearchBar = ({setResults}) => {   
   const [input, setInput] = useState("");
 
-  const fetchData = (value) => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.json())
-    .then((json) => {
-            const results = json.filter((user) => {
-            return value && user && user.name && user.name.toLowerCase().includes(value)
-        });
-        setResults(results)
-    });
-  };
+    //   const fetchData = (value) => {
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //             const results = json.filter((user) => {
+    //             return value && user && user.name && user.name.toLowerCase().includes(value)
+    //         });
+    //         setResults(results)
+    //     });
+    //   };
+  const filterData = (value) => {
+    const results = data.filter((course) => {
+        return (
+            value &&
+            course &&
+            ((course.course_code && 
+            course.course_code.toLowerCase().includes(value.toLowerCase())) ||
+            (course.course_name && 
+            course.course_name.toLowerCase().includes(value.toLowerCase()))) 
+        )
+    })
+    setResults(results);
+  }
 
   const handleChange = (value) => {
     setInput(value)
-    fetchData(value)
+    //fetchData(value)
+    filterData(value)
+    console.log(results)
   };
 
   return (
--- src/components/SearchResultList.jsx
+++ src/components/SearchResultList.jsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
+import { Table } from "react-bootstrap";
 
 export const SearchResultList = ({ results }) => {
     return (
         <div className="search-results">
-            {
-                results.map((result, id) => {
-                    return <div key={id}>{result.name}</div>
-                })
-            }
+             <Table>
+                <thead>
+                    <tr>
+                    <th>Course Code</th>
+                    <th>Course Name</th>
+                    <th>Professor</th>
+                    <th>Points</th>
+                    </tr>
+                </thead>
+    
+                <tbody>
+                    {results.map((item) => (
+                    <tr key={item.faculty+item.code}>
+                        <td>{item.course_code}</td>
+                        <td>{item.course_name}</td>
+                        <td>{item.professor_name}</td>
+                        <td>{item.points}</td>
+                    </tr>
+                    ))}
+                    
+                </tbody>
+            </Table>
         </div>
     )
 }
+
+// results.map((result, id) => {
+                //     return <div key={id}>{result.name}</div>
+                // })
