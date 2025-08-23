465d6ba30e777aac3b0b4cacf8a1efbb1d8c1730
jigsaw237 <snigdhamundra2@gmail.com>
2025-08-24 02:06:45 +1200

    Added DegreePlanner, graph forward planner that changes colours and draws the edges when selected

--- src/data/prereq.js
+++ src/data/prereq.js
@@ -1,108 +1,50 @@
 export const courseData = {
-  "courses": [
+  courses: [
+    { id: "COMP102", name: "", prerequisites: [] },
+    { id: "COMP103", name: "", prerequisites: ["COMP102", "COMP112"] },
+    { id: "COMP132", name: "", prerequisites: [] },
     {
-      "id": "ENGR101",
-      "name": "Engineering Technology",
-      "prerequisites": []
+      id: "COMP261",
+      name: "",
+      prerequisites: ["COMP103", "ENGR123", "MATH161"],
     },
+    { id: "CYBR171", name: "", prerequisites: [] },
+    { id: "CYBR271", name: "", prerequisites: ["CYBR171", "NWEN241"] },
+    { id: "CYBR371", name: "", prerequisites: ["CYBR171", "NWEN243"] },
     {
-      "id": "COMP102",
-      "name": "Introduction to Computer Program Design",
-      "prerequisites": []
+      id: "CYBR372",
+      name: "",
+      prerequisites: ["CYBR271", "NWEN243", "ENGR123", "MATH161", "STAT193"],
     },
+    { id: "CYBR373", name: "", prerequisites: ["CYBR271"] },
+    { id: "ENGR101", name: "", prerequisites: [] },
     {
-      "id": "COMP103",
-      "name": "Introduction to Data Structures and Algorithms",
-      "prerequisites": ["COMP102"]
+      id: "ENGR110",
+      name: "",
+      prerequisites: ["COMP102", "COMP112", "ENGR101"],
     },
+    { id: "ENGR201", name: "", prerequisites: ["ENGR101", "ENGR110"] },
+    { id: "ENGR301", name: "", prerequisites: ["ENGR201"] },
+    { id: "ENGR302", name: "", prerequisites: ["ENGR301"] },
+    { id: "ENGR121", name: "", prerequisites: [] },
+    { id: "ENGR122", name: "", prerequisites: ["ENGR121", "MATH141"] },
+    { id: "ENGR123", name: "", prerequisites: ["ENGR121"] },
     {
-      "id": "ENGR110",
-      "name": "Engineering Design",
-      "prerequisites": ["ENGR101"]
+      id: "CGRA151",
+      name: "",
+      prerequisites: ["COMP102", "COMP112", "DSDN142"],
     },
+    { id: "NWEN241", name: "", prerequisites: ["COMP103"] },
+    { id: "NWEN243", name: "", prerequisites: ["COMP103"] },
+    { id: "SWEN221", name: "", prerequisites: ["COMP103"] },
+    { id: "SWEN225", name: "", prerequisites: ["SWEN221"] },
+    { id: "SWEN301", name: "", prerequisites: ["SWEN225"] },
+    { id: "SWEN303", name: "", prerequisites: ["COMP261", "SWEN221"] },
+    { id: "SWEN326", name: "", prerequisites: ["NWEN241", "SWEN225"] },
     {
-      "id": "ENGR121",
-      "name": "Engineering Mathematics Foundations",
-      "prerequisites": []
+      id: "SWEN304",
+      name: "",
+      prerequisites: ["COMP261", "SWEN221", "ENGR123", "MATH161"],
     },
-    {
-      "id": "ENGR123",
-      "name": "Engineering Mathematics with Logic and Statistics ",
-      "prerequisites": ["ENGR121"]
-    },
-    {
-      "id": "CYBR171",
-      "name": "Cybersecurity Fundamentals",
-      "prerequisites": []
-    },
-    {
-      "id": "CGRA151",
-      "name": "Introduction to Computer Graphics and Games",
-      "prerequisites": ["COMP102"]
-    },
-    {
-      "id": "COMP261",
-      "name": "Algorithms and Data Structures",
-      "prerequisites": ["COMP103", "ENGR123"]
-    },
-    {
-      "id": "CYBR271",
-      "name": "	Code Security",
-      "prerequisites": ["CYBR171", "NWEN241"]
-    },
-    {
-      "id": "NWEN241",
-      "name": "Systems Programming",
-      "prerequisites": ["COMP103"]
-    },
-    {
-      "id": "NWEN243",
-      "name": "Clouds and Networking",
-      "prerequisites": ["COMP103"]
-    },
-    {
-      "id": "SWEN221",
-      "name": "Software Development",
-      "prerequisites": ["COMP103"]
-    },
-    {
-      "id": "SWEN225",
-      "name": "Software Design",
-      "prerequisites": ["SWEN221"]
-    },
-    {
-      "id": "ENGR201",
-      "name": "Engineering in Context",
-      "prerequisites": ["ENGR101", "ENGR110"]
-    },
-    {
-      "id": "ENGR301",
-      "name": "Engineering Project Management 1",
-      "prerequisites": ["ENGR201"]
-    },
-    {
-      "id": "ENGR302",
-      "name": "	Engineering Project Management 2",
-      "prerequisites": ["ENGR301"]
-    },
-    {
-      "id": "SWEN301",
-      "name": "Scalable Software Development",
-      "prerequisites": ["SWEN225"]
-    },
-    {
-      "id": "SWEN303",
-      "name": "User Experience Engineering",
-      "prerequisites": ["COMP261", "SWEN221"]
-    },
-    {
-      "id": "SWEN326",
-      "name": "Safety-Critical System",
-      "prerequisites": ["NWEN241", "SWEN225"]
-    }, 
-    {
-      "id": "SWEN304",
-      "name": "Database System Engineering",
-      "prerequisites": ["COMP261", "SWEN221"]
-    }
-  ]};
+  ],
+};
